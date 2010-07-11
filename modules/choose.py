89e09b155f43debdeccc4b7d479f5ffddbba256a
Selig Arkin <seligarkin@gmail.com>
2010-07-10 23:38:27 -0400

    Made choose use ' or ' instead of 'or'

--- modules/choose.py
+++ modules/choose.py
@@ -4,7 +4,7 @@ from random import choice
 def onLoad():
 	@command("choose")
 	def choose_cmd(ctx, cmd, arg):
-		ctx.reply("%s: %s" % (ctx.who.nick, choice(arg.split('or')).strip()))
+		ctx.reply("%s: %s" % (ctx.who.nick, choice(arg.split(' or ')).strip()))
 	
 	@command("8ball")
 	def eightball_cmd(ctx, cmd, arg):
