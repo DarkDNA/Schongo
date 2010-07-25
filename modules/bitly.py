274cff15d9f3a559cba1814deb64a414164e1590
WilHall <wil@wilhall.com>
2010-07-25 01:36:31 -0400

    Added 'bitly' module.

---
+++ modules/bitly.py
@@ -0,0 +1,31 @@
+"""
+Adds the 'bitly' command, which takes 1 URL as an argument. If the URL is not a bitly URL, it returns a bitly URL linked to the given URL. If the URL given is a bitly URL, it returns the real URL which the given bitly URL links to.
+"""
+
+import urllib2
+import urllib
+
+__info__ = {
+    "Author": "Wil Hall",
+    "Version": "1.0",
+    "Dependencies": []
+}
+
+def onLoad():
+    @command('bitly', 1)
+    def bitly_cmd(ctx, cmd, arg, *args):
+        args = arg.split(' ')
+        if len(args) > 0:
+            if 'http://bit.ly/' in args[0]:
+                try:
+                    request = urllib2.Request(args[0])
+                    opener = urllib2.build_opener()
+                    response = opener.open(request)
+                    url = response.url
+                    ctx.reply(u"\u0002Actual URL:\u0002 %s" % url, 'bitly')
+                except URLError:
+                    ctx.reply(u"\u0002Error:\u0002 Unable to open URL \u0002%s\u0002." % args[0], 'bitly')
+            else:
+                response = urllib2.urlopen("http://api.bit.ly/v3/shorten?login=wilatkathbot3&apiKey=R_8fa4afa5dda9ae6cab93896eaf04c8de&longUrl=%s&format=txt" % urllib.quote(args[0], ':/').strip().rstrip())
+                url = response.read()
+                ctx.reply(u"\u0002bit.ly URL:\u0002 %s" % url, 'bitly')
