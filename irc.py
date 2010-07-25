280a4bf73f5db6f0b3d901a37328cceffd44a13e
Selig Arkin <SeligArkin@gmail.com>
2010-07-25 16:09:29 +0100

    Fixed potential crash in unicode en/decoding - removed msgFormat

--- irc.py
+++ irc.py
@@ -64,13 +64,6 @@ class IrcSocket(Thread):
 		self._socket.close()
 		pass
     
-	def msgFormat(self, msg):
-		fmtchars = {
-        '`B':u'\u0002',
-        '`U':u'\u0001',
-        '`R':u'\u0016'}
-        off = '\u000F'
-    
 	def sendMessage(self, *msg, **kwargs):
 
 		if not self.connected:
@@ -91,7 +84,8 @@ class IrcSocket(Thread):
 		try:
 			line = line.encode('utf-8')
 		except UnicodeEncodeError, error:
-			logger.warn('Could not encode UTF-8 string sent to socket: %s' % error)
+			self.logger.warn('Could not encode UTF-8 string sent to socket: %s' % error)
+
 		self._socket.send(line)
 	
 	def run(self):
@@ -114,7 +108,7 @@ class IrcSocket(Thread):
 				try:
 					line = line.decode('utf-8')
 				except UnicodeDecodeError, error:
-					logger.warn('Could not decode UTF-8 string sent by socket: %s' % error)
+					self.logger.warn('Could not decode UTF-8 string sent by socket: %s' % error)
 				#print("=>" + line);
 				self.logger.debug("=> %s" % line)
 				self.onLine(line)
