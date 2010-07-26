ea94c3f966174175f2d169dd9d1b57bb89e54495
Selig Arkin <SeligArkin@gmail.com>
2010-07-26 20:05:52 +0100

    Changed the geoip module to use xml dom parsage instead of string replacement

--- modules/geoip.py
+++ modules/geoip.py
@@ -1,8 +1,8 @@
-"""
-Adds the 'geoip' command, which attempts to resolve a location for an IP or hostname.
+"""Adds the 'geoip' command, which attempts to resolve a location for an IP or hostname.
 """
 
 import urllib2
+import xml.dom.minidom as dom
 
 __info__ = {
 	"Author": "Wil Hall",
@@ -22,50 +22,70 @@ def onLoad():
 			#host = m('chantrack').network(irc)[channel.lower()].users[ip.lower()].hostname
 			host = host.split('@')
 			ip = host[0]
+
 		response = urllib2.urlopen('http://ipinfodb.com/ip_query2.php?ip=%s&timezone=false' % ip)
-		geo = response.read()
-		geo = geo.replace("""<?xml version="1.0" encoding="UTF-8"?>
+
+		xml = dom.parse(response)
+
+		ip = xml.getElementsByName("Ip")[0].firstChild.data
+		status = xml.getElementsByName("Status")[0].firstChild.data
+		cc = xml.getElementsByName("CountryCode")[0].firstChild.data
+		country = xml.getElementsByName("CountryName")[0].firstChild.data
+		region = xml.getElementsByName("RegionName")[0].firstChild.data
+		city = xml.getElementsByName("City")[0].firstChild.data
+		zip = xml.getElementsByName("ZipPostalCode")[0].firstChild.data
+		lat = xml.getElementsByName("Latitude")[0].firstChild.data
+		lon = xml.getElementsByName("Longitude")[0].firstChild.data
+
+		xml.unlink() # Phew! Release all the xml tree data now, since we just spent the code pulling it into vars. :)
+
+
+		lon = lon.replace('\n', '').replace('\\n', '')
+		ctx.reply(u"The IP Address `B%s`B %s traces to `B%s`B, `B%s`B, `B%s`B(`B%s`B) `B%s`B (`B%s`B, `B%s`B)." % (ip, fornick, city, region, country, cc, zip, lat, lon), 'geoip')
+
+
+def oldParse(response):
+	geo = response.read()
+	geo = geo.replace("""<?xml version="1.0" encoding="UTF-8"?>
 <Locations>
   <Location id="0">
     <Ip>""", '')
-		ip = geo[0:geo.find('<')]
-		geo = geo[geo.find('<'):]
-		geo = geo.replace("""</Ip>
+	ip = geo[0:geo.find('<')]
+	geo = geo[geo.find('<'):]
+	geo = geo.replace("""</Ip>
     <Status>""", '')
-		geo = geo[geo.find('<'):]
-		geo = geo.replace("""</Status>
+	geo = geo[geo.find('<'):]
+	geo = geo.replace("""</Status>
     <CountryCode>""", '')
-		cc = geo[0:geo.find('<')]
-		geo = geo[geo.find('<'):]
-		geo = geo.replace("""</CountryCode>
+	cc = geo[0:geo.find('<')]
+	geo = geo[geo.find('<'):]
+	geo = geo.replace("""</CountryCode>
     <CountryName>""", '')
-		country = geo[0:geo.find('<')]
-		geo = geo[geo.find('<'):]
-		geo = geo.replace("""</CountryName>
+	country = geo[0:geo.find('<')]
+	geo = geo[geo.find('<'):]
+	geo = geo.replace("""</CountryName>
     <RegionCode>""", '')
-		geo = geo[geo.find('<'):]
-		geo = geo.replace("""</RegionCode>
+	geo = geo[geo.find('<'):]
+	geo = geo.replace("""</RegionCode>
     <RegionName>""", '')
-		state = geo[0:geo.find('<')]
-		geo = geo[geo.find('<'):]
-		geo = geo.replace("""</RegionName>
+	state = geo[0:geo.find('<')]
+	geo = geo[geo.find('<'):]
+	geo = geo.replace("""</RegionName>
     <City>""", '')
-		city = geo[0:geo.find('<')]
-		geo = geo[geo.find('<'):]
-		geo = geo.replace("""</City>
+	city = geo[0:geo.find('<')]
+	geo = geo[geo.find('<'):]
+	geo = geo.replace("""</City>
     <ZipPostalCode>""", '')
-		zip = geo[0:geo.find('<')]
-		geo = geo[geo.find('<'):]
-		geo = geo.replace("""</ZipPostalCode>
+	zip = geo[0:geo.find('<')]
+	geo = geo[geo.find('<'):]
+	geo = geo.replace("""</ZipPostalCode>
     <Latitude>""", '')
-		lat = geo[0:geo.find('<')]
-		geo = geo[geo.find('<'):]
-		geo = geo.replace("""</Latitude>
+	lat = geo[0:geo.find('<')]
+	geo = geo[geo.find('<'):]
+	geo = geo.replace("""</Latitude>
     <Longitude>""", '')
-		lon = geo[0:geo.find('<')]
-		geo = geo[geo.find('<'):]
-		geo = geo.replace("""/Longitude>
+	lon = geo[0:geo.find('<')]
+	geo = geo[geo.find('<'):]
+	geo = geo.replace("""/Longitude>
   </Location>
 </Locations>""", '')
-		lon = lon.replace('\n', '').replace('\\n', '')
-		ctx.reply(u"The IP Address `B%s`B %s traces to `B%s`B, `B%s`B, `B%s`B(`B%s`B) `B%s`B (`B%s`B, `B%s`B)." % (ip, fornick, city, state, country, cc, zip, lat, lon), 'geoip')
