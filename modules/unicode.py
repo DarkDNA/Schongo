698f9d976d8e98729c05b3639dadcf5b37e6b875
Selig Arkin <SeligArkin@gmail.com>
2010-07-29 22:22:16 +0100

    Added unicode module

---
+++ modules/unicode.py
@@ -0,0 +1,62 @@
+"""Looks up information using the unicode database of happy! Yayyy!"""
+
+import unicodedata
+
+abbrTable = {
+	"Lu": "Uppercase Letter",
+	"Ll": "Lowercase Letter",
+	"Lt": "Titlecase Letter",
+	"Lo": "Other Letter",
+	"Mn": "Nonspacing Mark",
+	"Mc": "Spacing Mark",
+	"Me": "Enclosing Mark",
+	"Nd": "Decimal Number",
+	"Nl": "Letter Number",
+	"No": "Other Number",
+	"Pc": "Connector Puncuation",
+	"Pd": "Dash Puncuation",
+	"Ps": "Open Puncuation",
+	"Pe": "Close Puncuation",
+	"Pi": "Initial Puncuation",
+	"Pf": "Final Puncuation",
+	"Po": "Other Puncuation",
+	"Sm": "Math Symbol",
+	"Sc": "Currency Symbol",
+	"Sk": "Modifier Symbol",
+	"So": "Other Symbol",
+	"Zs": "Space Seperator",
+	"Zl": "Line Seperator",
+	"Zp": "Paragraph Seperator",
+	"Cc": "Control Char",
+	"Cf": "Format Char",
+	"Cs": "Surrogate Char",
+	"Co": "Private Use Char",
+	"Cn": "Unassigned"
+}
+
+def onLoad():
+	@command("unicode", 1, 1)
+	def unicode_cmd(ctx, cmd, arg, charOrSearch, *args):
+		"""unicode <char or search>
+Searches for information on <char> or does an exact-match search for a char called <search>"""
+		if len(charOrSearch) == 1:
+			# Likly a search-by-glyph
+			char = charOrSearch[0]
+		else:
+			try:
+				char = unicodedata.lookup(arg)
+			except:
+				ctx.error("Unknown char name %s" % arg)
+				return
+
+		name = unicodedata.name(char, "Unknown Unicode Char").title()
+		cat = unicodedata.category(char)
+		num = ord(char)
+		
+		if abbrTable.has_key(cat):
+			cat = abbrTable[cat]
+		else:
+			cat = "Unknown Category(%s)" % cat
+
+		ctx.reply(u"Char %s - U+%04X -  %s - %s" % (char, num, name, cat), "Unicode")
+			
