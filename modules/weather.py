17a719c957dfda75ff9589b29aa4894e83d7d299
Ross Delinger <posiden@Carthage.(none)>
2010-08-04 11:21:39 -0400

    Added a weather module, it only gets the forcast for today and tonight

---
+++ modules/weather.py
@@ -0,0 +1,31 @@
+# coding=utf-8
+"""
+A weather forcast module that works using the weather underground api
+"""
+import xml.dom.minidom as dom
+import urllib2
+
+__info__ = {
+	"Author": "Ross Delinger",
+	"Version": "1.0",
+	"Dependencies": []
+}
+
+weatherURL = 'http://api.wunderground.com/auto/wui/geo/ForecastXML/index.xml?query=%s'
+def onLoad():
+	
+	@command("weather", 1,1)
+	def weather(ctx, cmd, arg, args):
+			url = weatherURL % arg
+			page = urllib2.urlopen(url)
+			xml = dom.parse(page)
+			titles = xml.getElementsByTagName('title')
+			forcasts = xml.getElementsByTagName('fcttext')
+			count = 0
+			wDict = {}
+			for t in titles:
+				wDict[t.firstChild.data] = forcasts[count].firstChild.data
+				count += 1
+			
+			for title in wDict:
+				ctx.reply("%s: %s" % (title, wDict[title]), "Weather")
