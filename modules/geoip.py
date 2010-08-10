b2de1f38daffb89e37fc8276ed130aca7b58f427
wilhall <wil@wilhall.com>
2010-08-10 14:08:27 -0400

    Removed oldParse function because it sucked (My fault) and removes some unused code under geoip by nick (still not supported)

--- modules/geoip.py
+++ modules/geoip.py
@@ -17,10 +17,6 @@ def onLoad():
 		if '.' not in ip:
 			ctx.reply(u'geoip by Nick not currently supported.', 'geoip')
 			return
-			fornick = u'for `B%s`B' % ip
-			#host = m('chantrack').network(irc)[channel.lower()].users[ip.lower()].hostname
-			host = host.split('@')
-			ip = host[0]
 
 		response = urllib2.urlopen('http://ipinfodb.com/ip_query2.php?ip=%s&timezone=false' % ip)
 
@@ -41,50 +37,3 @@ def onLoad():
 
 		lon = lon.replace('\n', '').replace('\\n', '')
 		ctx.reply(u"The IP Address `B%s`B %s traces to `B%s`B, `B%s`B, `B%s`B(`B%s`B) `B%s`B (`B%s`B, `B%s`B)." % (ip, fornick, city, region, country, cc, zip, lat, lon), 'geoip')
-
-
-def oldParse(response):
-	geo = response.read()
-	geo = geo.replace("""<?xml version="1.0" encoding="UTF-8"?>
-<Locations>
-  <Location id="0">
-    <Ip>""", '')
-	ip = geo[0:geo.find('<')]
-	geo = geo[geo.find('<'):]
-	geo = geo.replace("""</Ip>
-    <Status>""", '')
-	geo = geo[geo.find('<'):]
-	geo = geo.replace("""</Status>
-    <CountryCode>""", '')
-	cc = geo[0:geo.find('<')]
-	geo = geo[geo.find('<'):]
-	geo = geo.replace("""</CountryCode>
-    <CountryName>""", '')
-	country = geo[0:geo.find('<')]
-	geo = geo[geo.find('<'):]
-	geo = geo.replace("""</CountryName>
-    <RegionCode>""", '')
-	geo = geo[geo.find('<'):]
-	geo = geo.replace("""</RegionCode>
-    <RegionName>""", '')
-	state = geo[0:geo.find('<')]
-	geo = geo[geo.find('<'):]
-	geo = geo.replace("""</RegionName>
-    <City>""", '')
-	city = geo[0:geo.find('<')]
-	geo = geo[geo.find('<'):]
-	geo = geo.replace("""</City>
-    <ZipPostalCode>""", '')
-	zip = geo[0:geo.find('<')]
-	geo = geo[geo.find('<'):]
-	geo = geo.replace("""</ZipPostalCode>
-    <Latitude>""", '')
-	lat = geo[0:geo.find('<')]
-	geo = geo[geo.find('<'):]
-	geo = geo.replace("""</Latitude>
-    <Longitude>""", '')
-	lon = geo[0:geo.find('<')]
-	geo = geo[geo.find('<'):]
-	geo = geo.replace("""/Longitude>
-  </Location>
-</Locations>""", '')
