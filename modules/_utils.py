63ac4de413e9c353bb2440a8ba62a1bbff394423
wilhall <wil@wilhall.com>
2010-08-10 14:07:11 -0400

    Added 'stripPunctuaton', 'casecor', and 'listify' functions.

--- modules/_utils.py
+++ modules/_utils.py
@@ -24,6 +24,37 @@ def prettyTime(secs):
 
 	return time
 
+#Strips everything but letters and numbers from a string. If 'exceptions' is included as a string, any character in exceptions will be left as-is.
+def stripPunctuation(str, exceptions=''):
+	letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+	numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+	exceptions = exceptions.split('').extend(letters).extend(numbers)
+	nstr = ''
+	for char in str:
+		if char in exceptions:
+			nstr += char
+	return nstr
+
+#Capitalizes first letter of the string and makes the rest lower case. Does this for the first character after every space until it hits another space if atspaces is set to True.
+def casecor(str, atspaces=False):
+		if atspaces:
+			cor = ''
+			pieces = str.split(' ')
+			for piece in pieces:
+				cor += ' ' + piece[0:1].upper() + piece[1:].lower()
+			return cor[1:]
+		else:
+			   return cor[0:1].upper() + cor[1:].lower()
+
+#Properly format out a list to a string, including separating commas and 'and' before the last value if useand set to true.
+def listify(list, useand=False):
+	if len(list) == 1:
+		return list[0]
+	result = ', '.join(list)
+	if useand:
+		result = result[0:result.rfind(',')+2] + 'and' + result[result.rfind(',')+1:]
+	return result
+
 class procThread(threading.Thread):
 	context = None
 	proc = None
