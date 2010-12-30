fb02d6a59bb2cd6df9a7b33f5eec5dde1a9b7a5f
Selig Arkin <SeligArkin@gmail.com>
2010-12-30 17:55:31 -0500

    Make weather.py able to select from nearby airports

--- modules/weather.py
+++ modules/weather.py
@@ -3,7 +3,7 @@
 A weather forcast module that works using the weather underground api
 """
 import xml.dom.minidom as dom
-import urllib2
+import urllib
 
 __info__ = {
 	"Author": "Ross Delinger",
@@ -11,14 +11,45 @@ __info__ = {
 	"Dependencies": []
 }
 
-weatherURL = 'http://api.wunderground.com/auto/wui/geo/ForecastXML/index.xml?query=%s'
+forecastURL = 'http://api.wunderground.com/auto/wui/geo/ForecastXML/index.xml?query=%s'
+lookupURL = 'http://api.wunderground.com/auto/wui/geo/GeoLookupXML/index.xml?query=%s'
+weatherURL = 'http://api.wunderground.com/auto/wui/geo/WXCurrentObXML/index.xml?query=%s'
+
+airportCache = {}
+
 def onLoad():
 	
-	@command("weather", 1,1)
-	def weather(ctx, cmd, arg, args):
+	@command("weather", 1)
+	def weather(ctx, cmd, arg, *args):
+		""" weather <location>
+		Gives the weather for the given location, powered by Weather Underground.
+		"""
+		# TODO: Better caching -- also, personal weather stations should be included and the closest to the input given selected
+
+		if arg in airportCache:
+			airport = airportCache[arg]
+		else:
+			xml = dom.parse(urllib.urlopen(lookupURL % arg))
+			airport = xml.getElementsByTagName("station")[0].getElementsByTagName("icao")[0].firstChild.data
+
+			airportCache[arg] = airport
+
+		xml = dom.parse(urllib.urlopen(weatherURL % airport))
+
+		resp = u"Current Conditions from %s" % xml.getElementsByTagName("observation_location")[0].getElementsByTagName("full")[0].firstChild.data
+
+		resp += u" • %s" % xml.getElementsByTagName("weather")[0].firstChild.data
+		resp += u" • Temperature: %s" % xml.getElementsByTagName("temperature_string")[0].firstChild.data
+		resp += u" • Winds: %s" % xml.getElementsByTagName("wind_string")[0].firstChild.data
+
+		ctx.reply(resp, "Weather")
+
+
+	@command("forecast", 1, 1)
+	def forecast(ctx, cmd, arg, *args):
 			"""weather <city>\nReturns the current forcast for the given area should be able to take area code or city"""
-			url = weatherURL % arg
-			page = urllib2.urlopen(url)
+			url = forecastURL % arg
+			page = urllib.urlopen(url)
 			xml = dom.parse(page)
 			titles = xml.getElementsByTagName('title')
 			forcasts = xml.getElementsByTagName('fcttext')
