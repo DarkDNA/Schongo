d8af358c98f4ee68705cc6cd65c45643e7b02c81
Selig Arkin <seligarkin@gmail.com>
2011-06-25 16:05:14 -0400

    Forgot to uncomment the shutdown limiter

--- modules/__init__.py
+++ modules/__init__.py
@@ -537,7 +537,7 @@ Lists the currently running threads"""
 	ctx.reply(s, "Info")
 
 
-#@priv(5)
+@priv(5)
 @command("shutdown")
 def shutdown_cmd(ctx, cmd, arg):
 	"""Shuts down the bot with the given message"""
