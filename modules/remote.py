40436dd01703c5ec6182f265c0e6b5e8a115868b
Selig Arkin <seligarkin@gmail.com>
2011-07-06 19:01:04 -0400

    Add permissions to remote module.

--- modules/remote.py
+++ modules/remote.py
@@ -6,15 +6,12 @@ def onLoad():
 	def ping_cmd(ctx, cmd, arg):
 		ctx.reply("Pong!")
 
+	@privs(2)
 	@command("say")
 	def say_cmd(ctx, cmd, arg):
 		ctx.reply(arg)
 	
-	@command("hello", 1)
-	def hello_cmd(ctx, cmd, arg, who):
-		ctx.reply("Hello, %s!" % who)
-		
-	
+	@privs(5)
 	@command("eval")
 	def eval_cmd(ctx, cmd, arg):
 		exec arg
