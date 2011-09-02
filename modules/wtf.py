87b0780d825bc95a31a70378d81ce91299656c6d
Selig Arkin <seligarkin@gmail.com>
2011-09-01 22:52:34 -0400

    Added 'port' command that searches the nmap port database

--- modules/wtf.py
+++ modules/wtf.py
@@ -6,11 +6,12 @@ This command looks up common acronyms.
 import glob
 import re
 
-splitter = re.compile("([A-Z]+)\s+(.+)")
+splitter = re.compile("(.+)\t([0-9])/(sctp|tcp|udp)\t([0-9]\.[0-9]+).+(#.+)?")
 
 def onLoad():
 	acronyms = {}
-	
+	ports = {}
+
 	for db in glob.glob("/usr/share/misc/acronyms*"):
 		with open(db, "r") as f:
 			for line in f:
@@ -24,6 +25,48 @@ def onLoad():
 						pass
 				acronyms[acronym] = meaning.strip()
 	
+	n = 0
+	try:
+		with file("/usr/share/nmap/nmap-services", "r") as f:
+			for line in f:
+				line = line.strip()
+				if line[0] == "#":
+					continue
+				"""g = splitter.match(line)
+				if g is None:
+					n += 1
+					if n < 5:
+						logger.error("Invalid line: %s", line)
+					continue
+				name = g.group(1)
+				port = int(g.group(2))
+				which = g.group(3)
+				comment = g.group(5)"""
+
+				try:
+					line, comment = line.split("#")
+				except:
+					comment = "No comment"
+
+				name, portproto, _ = line.split("\t", 2)
+
+				port, proto = portproto.split("/")
+
+				port = int(port)
+
+				if proto not in ports:
+					ports[proto] = {}
+
+				ports[proto][port] = (name, comment)
+
+		for proto in ports:
+			logger.info("Found %d ports for %s", len(ports[proto]), proto)
+
+	except IOError:
+		logger.error("the port command will not be supported because we could not find the nmap service database")
+	
+
+	
 	@command("wtf", 1, 1)
 	def wtf_cmd(ctx, cmd, arg, word):
 		word = word.upper()
@@ -31,5 +74,11 @@ def onLoad():
 			ctx.reply("%s: %s" % (word, acronyms[word]), "WTF")
 		else:
 			ctx.error("I don't know what %s means" % word)
-
-
+	
+	if len(ports) != 0:
+		@command("port", 1, 1)
+		def port_cmd(ctx, cmd, arg, port):
+			port = int(port)
+			for proto in ports:
+				if port in ports[proto]:
+					ctx.reply("%s: %s" % ports[proto][port], proto)
