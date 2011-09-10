306ffb9ce7d558f6896f644653d940ef2ed24a96
posiden <posiden@helixoide.com>
2011-09-10 13:51:09 -0400

    the wikipedia module is now python3 compliant

--- modules/wikipedia.py
+++ modules/wikipedia.py
@@ -3,16 +3,16 @@
 
 Please note that this is still in development and needs some kinks to be worked out still"""
 
-import urllib
-import urllib2
+import urllib.request
+import urllib.parse
 import xml.dom.minidom as dom
 import re
-from _utils import prettyNumber
+from modules._utils import prettyNumber
 
 def openUrl(url):
-	r = urllib2.Request(url, headers={
+	r = urllib.request.Request(url, headers={
 		"User-Agent": "Schongo Bot"})
-	return urllib2.urlopen(r)
+	return urllib.request.urlopen(r)
 
 
 def onLoad():
@@ -20,7 +20,7 @@ def onLoad():
 	def wikipedia_cmd(ctx, cmd, arg):
 		"""wikipedia <search>
 Searches through wikipedia for the given search string"""
-		o = openUrl(u"http://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=%s&srwhat=text&srlimit=3&format=xml" % urllib.quote(arg))
+		o = openUrl("http://en.wikipedia.org/w/api.php?action=query&list=search&srsearch={}&srwhat=text&srlimit=3&format=xml".format(urllib.parse.quote(arg)))
 
 		xml = dom.parse(o)
 		
@@ -28,18 +28,18 @@ Searches through wikipedia for the given search string"""
 		
 		if results > 0:
 			res = min(results, 3)
-			ctx.reply("Results 1-%d out of %s" % (res, prettyNumber(results)), "Wikipedia")
+			ctx.reply("Results 1-{} out of {}".format(res, prettyNumber(results)), "Wikipedia")
 		else:
-			ctx.reply(u"No results found for %s" % arg, "Wikipedia")
+			ctx.reply("No results found for {}".format(arg), "Wikipedia")
 
 		for i in xml.getElementsByTagName("p"):
 			title = i.getAttribute("title")
 			snippet = i.getAttribute("snippet")
 			# Hilight the matched parts
-			snippet = re.sub('<span class=\'searchmatch\'>(.+?)</span>', u'`B\\1`B', snippet)
+			snippet = re.sub('<span class=\'searchmatch\'>(.+?)</span>', '`B\\1`B', snippet)
 			# Clean it up nice and pretty now
-			snippet = re.sub('<[^>]+>', u'', snippet)
-			snippet = re.sub(' ([.,!?\'])', u'\\1', snippet)
-			snippet = snippet.replace('  ', u' ')
+			snippet = re.sub('<[^>]+>', '', snippet)
+			snippet = re.sub(' ([.,!?\'])', '\\1', snippet)
+			snippet = snippet.replace('  ', ' ')
 			# And then display it. :D
-			ctx.reply(u"`B%s`B ( %s ) • %s" % (title, "http://wikipedia.org/wiki/%s" % urllib.quote(title), snippet), "Wikipedia")
+			ctx.reply("`B{}`B ( {} ) • {}".format(title, "http://wikipedia.org/wiki/{}".format(urllib.parse.quote(title)), snippet), "Wikipedia")
